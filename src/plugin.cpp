d20146050e880f9bc8c022b12a1eb49bbb2e0933
thorwe <philosound@gmail.com>
2013-11-25 12:46:29 +0100

    v1.5.2

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -133,7 +133,7 @@ const char* ts3plugin_name() {
 
 /* Plugin version */
 const char* ts3plugin_version() {
-    return "1.5.1.112301";
+    return "1.5.2";
 }
 
 /* Plugin API version. Must be the same as the clients API major version, else the plugin fails to load. */
