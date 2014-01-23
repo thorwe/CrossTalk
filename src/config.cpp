017cae97958b8088bf83592ac8b6e1768faca56d
thorwe <philosound@gmail.com>
2014-01-23 17:23:07 +0100

    cleanup

--- src/config.cpp
+++ src/config.cpp
@@ -1,7 +1,7 @@
 #include "config.h"
+#include "ui_config.h"
 
 #include <QtCore/QSettings>
-#include <QDialog>
 #include <QWhatsThis>
 #include <QDesktopServices>
 
@@ -14,9 +14,41 @@ const QUrl JIANJI_CAMPAIGN("http://www.jianji.de");
 /*!
  * \brief Config::Config create an instance of this class
  */
-Config::Config()
+Config::Config(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Config)
 {
-//    loca = Translator::instance();
+    ui->setupUi(this);
+    this->setAttribute( Qt::WA_DeleteOnClose );
+    this->setFixedSize(this->width(),this->height());
+
+    // load image
+    connect(ui->banner_jianji,SIGNAL(onClick()),SLOT(onJianjiClicked()));
+
+    QString lang = TSHelpers::GetLanguage();
+    if (lang != "de_DE") // outside of german locale, hide jianji banner
+        ui->banner_jianji->setVisible(false);
+
+    QSettings cfg(TSHelpers::GetFullConfigPath(), QSettings::IniFormat);
+    ui->betaChannelcheckBox->setChecked(cfg.value("beta",false).toBool());
+    connect(ui->betaChannelcheckBox,SIGNAL(toggled(bool)),SLOT(onBetaChannelToggled(bool)));
+
+    bool ok;
+    quint16 port = cfg.value("server_port",64736).toUInt(&ok);
+    if (!ok)
+    {
+        TSLogging::Error("Could not read port from settings");
+        ui->groupBox_Server->setChecked(false);
+        ui->groupBox_Server->setCheckable(false);
+        ui->spinBox_Server->setValue(0);
+    }
+    else
+    {
+        ui->spinBox_Server->setValue(port);
+        connect(ui->spinBox_Server,SIGNAL(valueChanged(int)),this,SLOT(onServerPortChanged(int)));
+        ui->groupBox_Server->setChecked(cfg.value("server_enabled",false).toBool());
+        connect(ui->groupBox_Server,SIGNAL(toggled(bool)),this,SLOT(onServerEnabledToggled(bool)));
+    }
 }
 
 //! Destructor
@@ -25,82 +57,7 @@ Config::Config()
  */
 Config::~Config()
 {
-}
-
-//! Translate all ui elements
-/*!
- * \brief Config::Translate called initially with this, reversing
- * \param obj this
- */
-/*void Config::Translate(QObject* obj)
-{
-    const QObjectList list = obj->children();
-    if (!list.isEmpty())
-    {
-        for (int i=0;i<list.size();++i)
-        {
-            Translate(list[i]);
-        }
-    }
-
-    QVariant test;
-    QStringList props;
-    props << "text" << "whatsThis" << "title";
-
-    for (int j=0;j<props.size();++j)
-    {
-        QString prop = props[j];
-        test = obj->property(prop.toLocal8Bit().constData());
-        if (test.isValid())
-        {
-            QString trans = test.toString();
-            if (!trans.isEmpty())
-            {
-                trans = loca->translator->translate("Config",trans.toLocal8Bit().constData());
-                if (!trans.isEmpty())
-                    obj->setProperty(prop.toLocal8Bit().constData(),trans.toLocal8Bit().constData());
-            }
-        }
-    }
-}*/
-
-//! Called when the widget should open
-/*!
- * \brief Config::SetupUi combine with .ui, initialize
- */
-void Config::SetupUi()
-{
-    setupUi(this);
-    this->setFixedSize(this->width(),this->height());
-    this->setAttribute( Qt::WA_DeleteOnClose );
-
-    // load image
-    connect(banner_jianji,SIGNAL(onClick()),this,SLOT(onJianjiClicked()));
-
-    QString lang = TSHelpers::GetLanguage();
-    if (lang != "de_DE") // outside of german locale, hide jianji banner
-        banner_jianji->setVisible(false);
-
-//    if ((loca->translator != NULL) && !(loca->translator->isEmpty()))
-//        Translate(this);
-    QSettings cfg(TSHelpers::GetFullConfigPath(), QSettings::IniFormat);
-    betaChannelcheckBox->setChecked(cfg.value("beta",false).toBool());
-    connect(betaChannelcheckBox,SIGNAL(toggled(bool)),SLOT(onBetaChannelToggled(bool)));
-
-    bool ok;
-    quint16 port = cfg.value("server_port",64736).toUInt(&ok);
-    if (!ok)
-    {
-        TSLogging::Error("Could not read port from settings");
-        groupBox_Server->setChecked(false);
-    }
-    else
-    {
-        spinBox_Server->setValue(port);
-        connect(spinBox_Server,SIGNAL(valueChanged(int)),this,SLOT(onServerPortChanged(int)));
-        groupBox_Server->setChecked(cfg.value("server_enabled",false).toBool());
-        connect(groupBox_Server,SIGNAL(toggled(bool)),this,SLOT(onServerEnabledToggled(bool)));
-    }
+    delete ui;
 }
 
 //! Receives the click of the jianji banner
--- src/config.h
+++ src/config.h
@@ -2,19 +2,18 @@
 #define CONFIG_H
 
 #include <QDialog>
-//#include <QtNetwork/QtNetwork>
-#include "ui_config.h"
-//#include "translator.h"
-//#include "banner.h"
 
-class Config : public QDialog, public Ui::Config
+namespace Ui {
+class Config;
+}
+
+class Config : public QDialog
 {
 	Q_OBJECT
 
 public:
-    Config();
+    explicit Config(QWidget *parent = 0);
     ~Config();
-    void SetupUi();
 
 protected slots:
     void onJianjiClicked();
@@ -22,18 +21,13 @@ protected slots:
     void onServerEnabledToggled(bool val);
     void onServerPortChanged(int val);
 
-protected:
-//    bool eventFilter(QObject *object, QEvent *ev);
-
 signals:
     void betaChannelToggled(bool);
     void serverEnabledToggled(bool);
     void serverPortChanged(quint16);
 
 private:
-//    Translator *loca;
-//    QNetworkAccessManager *m_netwManager;
-//    void Translate(QObject *obj);
+    Ui::Config *ui;
 };
 
 #endif // CONFIG_H
