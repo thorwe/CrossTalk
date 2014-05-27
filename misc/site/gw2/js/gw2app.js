e5b933b53f771a4ca59671dfbfd81542ee1d952c
thorwe <philosound@gmail.com>
2014-05-27 13:38:54 +0200

    debug

--- misc/site/gw2/js/gw2app.js
+++ misc/site/gw2/js/gw2app.js
@@ -199,6 +199,7 @@
             // if theres no marker for the current player, create a new one
             if(typeof m.playermarkers[key] === "undefined")
             {
+				console.log("Creating new marker");
                 //marker = L.circleMarker(m.map.unproject(pos, m.map.getMaxZoom()));    //, <Path options> options?
                 marker = L.marker(m.map.unproject(pos, m.map.getMaxZoom()), {title: name, icon: icon});
                 marker.bindPopup(popup,{closeButton:false,closeOnClick:true,keepInView:true});
@@ -295,6 +296,7 @@
             // else update the existing one
             else
             {
+				console.log("Updating existing marker");
                 marker = m.playermarkers[key];
                 marker.setLatLng(m.map.unproject(pos, m.map.getMaxZoom()));
                 marker.setPopupContent(popup,{closeButton:false,closeOnClick:true,keepInView:true});
