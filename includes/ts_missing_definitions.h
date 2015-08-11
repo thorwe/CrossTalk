93f2e54c50d23f9b74e7ef58f224bbe2d1c61e42
thorwe <philosound@gmail.com>
2015-08-11 13:42:51 +0200

    proper enclosure

--- includes/ts_missing_definitions.h
+++ includes/ts_missing_definitions.h
@@ -1,8 +1,6 @@
 #ifndef TS_MISSING_DEFINITIONS_H
 #define TS_MISSING_DEFINITIONS_H
 
-#endif // TS_MISSING_DEFINITIONS_H
-
 enum PermissionGroupDatabaseTypes {
     PermGroupDBTypeTemplate = 0, // 0: template group     (used for new virtual servers)
     PermGroupDBTypeRegular, // 1: regular group      (used for regular clients)
@@ -21,3 +19,5 @@ enum TokenType {
     TokenServerGroup = 0, // 0: server group token (id1={groupID} id2=0)
     TokenChannelGroup // 1: channel group token (id1={groupID} id2={channelID})
 };
+
+#endif // TS_MISSING_DEFINITIONS_H
